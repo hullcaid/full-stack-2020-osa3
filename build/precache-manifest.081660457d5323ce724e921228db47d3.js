e2f5a9189a6256a9f90be4fadd281ce807bf4939
Jussi Leinonen <hullcaid@gmail.com>
2020-05-12 20:58:33 +0300

    uibuild

---
+++ build/precache-manifest.081660457d5323ce724e921228db47d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bd9eb120bb9d82b3cf3142d341396a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79271a74faeddb8d8717",
+    "url": "/static/js/2.47263524.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.47263524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "086a60330b9266bdcb23",
+    "url": "/static/js/main.b50eba73.chunk.js"
+  },
+  {
+    "revision": "d00a55e58aad67ff89c6",
+    "url": "/static/js/runtime-main.01dd18a1.js"
+  }
+]);

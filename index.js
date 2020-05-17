4f669e46eb7fe7a3a7eab5833bb4c2a9c6c088ce
Jussi Leinonen <hullcaid@gmail.com>
2020-05-17 16:25:57 +0300

    Tehtävä 3.15

--- index.js
+++ index.js
@@ -34,9 +34,11 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	persons = persons.filter(person => person.id !== id)
-	response.status(204).end()
+	Contact.findByIdAndRemove(request.params.id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 const generateId = () => {
